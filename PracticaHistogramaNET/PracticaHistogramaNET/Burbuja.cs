f45ae4117c999f711a91179030fda138c3d5838c
Iván L. Murillo <diskman199@gmail.com>
2022-09-14 19:47:58 -0500

    Gráficos con C#

---
+++ PracticaHistogramaNET/PracticaHistogramaNET/Burbuja.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaHistogramaNET
+{
+    class Burbuja
+    {
+        private int[] conjunto;
+
+        public Burbuja(int[] conjunto)
+        {
+            this.conjunto = conjunto;
+        }
+
+        public int[] GetConjunto()
+        {
+            return this.conjunto;
+        }
+
+        public void Sort()
+        {
+            for (int i = 0; i < conjunto.Length - 1; i++)
+            {
+                for (int j = 0; j < conjunto.Length - i - 1; j++)
+                {
+                    if (conjunto[j] > conjunto[j + 1])
+                    {
+                        int temp = conjunto[j];
+                        conjunto[j] = conjunto[j + 1];
+                        conjunto[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ PracticaHistogramaNET/PracticaHistogramaNET/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace PracticaHistogramaNET
+{
+    public partial class Form1 : Form
+    {
+        int[] conjunto = { 5, 8, 9, 2, 7, 4, 2};
+        String[] datos = { "A", "B", "C", "D", "E", "F", "G"};
+        Burbuja burbuja;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.burbuja = new Burbuja(conjunto);
+        }
+
+        private void btnOrdenar_Click(object sender, EventArgs e)
+        {
+            burbuja.Sort();
+            this.conjunto = burbuja.GetConjunto();
+
+            Series series = this.chart1.Series.Add("Total");
+            this.chart1.Series.Clear();
+
+            this.chart1.Palette = ChartColorPalette.SeaGreen;
+            this.chart1.Titles.Add("Ventas");
+            for (int i = 0; i < conjunto.Length; i++)
+            {
+                series = this.chart1.Series.Add(datos[i]);
+                series.Points.Add(conjunto[i]);
+            }
+        }
+    }
+}
